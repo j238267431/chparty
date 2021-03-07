614757ccf5f12aed12b494d219acfb5db8a9567d
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2021-03-07 23:21:58 +0700

    test

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function index()
     {
-        return view('message');
+        $user_id = Auth::user()->id;
+        $messages = Message::where('from_user_id', $user_id)->orWhere('to_user_id', $user_id)->get();
+        return view('message', ['messages' => []]);
     }
+
     public function send(Request $request)
     {
-        $message = $request->message;
-        return view('sender', ['message' => $message]);
+//        $message = $request->message;
+//        if ($request->message) {
+//            $message = new Message();
+//            $message->fill($request->all())
+////            echo $message;
+//            // todo записать сообщение в базу данных
+//        }
+        $user_id = Auth::user()->id;
+        $messages = [
+            'from_user_id'=>$user_id,
+            'message'=>$request->message
+        ];
+        return view('sender', ['message' => $messages]);
     }
 }
--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,5 +1,12 @@
 <div>
-    <div id="message_answer" class="massage_answer"></div>
+    <div id="message_answer" class="massage_answer" style="overflow-y: auto">
+        @forelse($messages as $message)
+            <p class="font-weight-bold">От {{ $message->from_user_id }} к {{ $message->to_user_id }}</p>
+            <p>{{ $message->message}}</p>
+        @empty
+            <p>Нет сообщений</p>
+        @endforelse
+    </div>
     <textarea name="message" id="message_sender" cols="30" rows="10"></textarea>
     <div onclick="sendMessage()">отправить</div>
 </div>
